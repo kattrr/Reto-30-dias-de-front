08bc73866ca71d69d928218645febcb6089e9185
Kathering Rivera <katheriverar@gmail.com>
2025-04-30 21:48:34 -0500

    Añadir sección de reloj digital y estilos correspondientes :cat2:

---
+++ js/reloj.js
@@ -0,0 +1,11 @@
+function updateClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+  document.getElementById('time').textContent = formattedTime;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
