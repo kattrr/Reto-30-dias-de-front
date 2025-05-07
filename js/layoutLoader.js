accd49bdb88ac6d8f57d1a9d28eae3ef1bb1bf36
Kathering Rivera <katheriverar@gmail.com>
2025-05-07 15:27:41 -0500

    carga dinamica del nav y footer :cat2:

--- js/layoutLoader.js
+++ js/layoutLoader.js
@@ -1,18 +1,21 @@
-async function loadLayout() {
-  const navContainer = document.querySelector("nav");
-  const footerContainer = document.querySelector("footer");
+async function loadLayout(id, file) {
+  const element = document.getElementById(id);
+  if (element) {
+    try {
+      const response = await fetch(`../layout/${file}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-  if (navContainer) {
-    const navResponse = await fetch("../layout/nav.html");
-    const navContent = await navResponse.text();
-    navContainer.outerHTML = navContent;
+      const content = await response.text();
+      element.outerHTML = content;
+    }
+    catch (error) {
+      console.error('Error loading layout:', error);
+    }
   }
-
-  if (footerContainer) {
-    const footerResponse = await fetch("../layout/footer.html");
-    const footerContent = await footerResponse.text();
-    footerContainer.outerHTML = footerContent;
-  }
-}
-
-document.addEventListener("DOMContentLoaded", loadLayout);
+};
+document.addEventListener("DOMContentLoaded", () => {
+  loadLayout("nav", "nav.html");
+  loadLayout("footer", "footer.html");
+});
